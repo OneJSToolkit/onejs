2f7a4a7c134d4c4c84bd728a6afafab9e9dd100c
Jeff Fisher <jeffish@microsoft.com>
2014-09-26 16:29:24 -0700

    Allow VMs to set default in their constructor.
    This also required making sure that nested viewmodels get initialized so that the user-specified data would be honored.

--- dist/amd/ViewModel.d.ts
+++ dist/amd/ViewModel.d.ts
@@ -5,12 +5,14 @@ declare class ViewModel {
     public __events: EventGroup;
     private static __instanceCount;
     private __id;
+    private __initializationData;
     constructor(data?: any);
     public initialize(): void;
     public onInitialize(): void;
     public dispose(): void;
     public onDispose(): void;
     public setData(data: any, shouldFireChange?: boolean, forceListen?: boolean): void;
+    public __getDataKeys(data: any): string[];
     public change(args?: any): void;
 }
 export = ViewModel;
--- src/ViewModel.ts
+++ src/ViewModel.ts
@@ -9,14 +9,20 @@ class ViewModel {
 
     private static __instanceCount = 0;
     private __id = ViewModel.__instanceCount++;
+    private __initializationData:any;
 
     public constructor(data ? : any) {
         this.__events = new EventGroup(this);
         this.__events.declare('change');
-        this.setData(data);
+        this.__initializationData = data;
     }
 
     public initialize() {
+
+        if (this.__initializationData) {
+            this.setData(this.__initializationData);
+        }
+
         this.setData(this, false, true);
 
         for (var i = 0; i < this.parentValues.length; i++) {
@@ -34,6 +40,14 @@ class ViewModel {
             }
         }
 
+        this.__getDataKeys(this).forEach((key) => {
+            var value = this[key];
+            // ensure nested ViewModels are initialized
+            if (value && (typeof value.initialize === 'function')) {
+                value.initialize();
+            }
+        });
+
         this.onInitialize();
     }
 
@@ -49,32 +63,43 @@ class ViewModel {
     public setData(data: any, shouldFireChange ? : boolean, forceListen?: boolean) {
         var hasChanged = false;
 
-        for (var i in data) {
-            if (data.hasOwnProperty(i) &&
-                i.indexOf('__') !== 0 &&
-                i !== 'setData' &&
-                i !== 'dispose' &&
-                i !== 'change') {
+        this.__getDataKeys(data).forEach((key) => {
+            var oldValue = this[key];
+            var newValue = data[key];
 
-                var oldValue = this[i];
-                var newValue = data[i];
-
-                if (oldValue !== newValue || forceListen) {
-                    if (oldValue && EventGroup.isDeclared(oldValue, 'change')) {
-                        this.__events.off(oldValue);
-                    }
-                    this[i] = newValue;
-                    hasChanged = true;
-                    if (newValue && EventGroup.isDeclared(newValue, 'change')) {
-                        this.__events.on(newValue, 'change', this.change);
-                    }
+            if (oldValue !== newValue || forceListen) {
+                if (oldValue && EventGroup.isDeclared(oldValue, 'change')) {
+                    this.__events.off(oldValue);
+                }
+                this[key] = newValue;
+                hasChanged = true;
+                if (newValue && EventGroup.isDeclared(newValue, 'change')) {
+                    this.__events.on(newValue, 'change', this.change);
                 }
             }
-        }
+        });
 
         if ((hasChanged && shouldFireChange !== false) || shouldFireChange === true) {
             this.__events.raise('change');
         }
+    }
+
+    public __getDataKeys(data: any):string[] {
+        return Object.keys(data).filter(function (key) {
+            var valid = true;
+
+            if (key.indexOf('__') === 0) {
+                valid = false;
+            } else if (key === 'setData') {
+                valid = false;
+            } else if (key === 'dispose') {
+                valid = false;
+            } else if (key === 'change') {
+                valid = false;
+            }
+
+            return valid;
+        });
     }
 
 
