df025b18fdf85143bf9427fce37c69c8f224c44d
JD Huntington <jdhuntington@gmail.com>
2014-10-11 15:22:59 -0700

    Tests for DomUtils.toggleClass

---
+++ test/DomUtils.test.ts
@@ -0,0 +1,45 @@
+/// <reference path="../definitions/definitions.d.ts" />
+
+import chai = require("chai");
+import List = require("../src/List");
+import DomUtils = require("../src/DomUtils");
+
+var expect = chai.expect;
+
+describe('DomUtils', function() {
+    describe('toggleClass', () => {
+        var el: HTMLDivElement;
+
+        beforeEach(() => {
+            el = document.createElement('div');
+        });
+
+        describe('when isEnabled=true', () => {
+            it('adds a class to a DOM element with no classes', () => {
+                DomUtils.toggleClass(el, 'foo', true);
+                expect(el.className).to.equal('foo');
+            });
+
+            it('adds a class to a DOM element with some classes', () => {
+                el.className = 'bar'
+                DomUtils.toggleClass(el, 'foo', true);
+                expect(el.className).to.equal('bar foo');
+            });
+
+        });
+
+        describe('when isEnabled=false', () => {
+            it('does not add a class to a DOM element with no classes', () => {
+                DomUtils.toggleClass(el, 'foo', false);
+                expect(el.className).to.equal('');
+            });
+
+            it('removes a class from a DOM element', () => {
+                el.className = 'foo bar'
+                DomUtils.toggleClass(el, 'foo', false);
+                expect(el.className).to.equal('bar');
+            });
+        });
+    });
+
+});
