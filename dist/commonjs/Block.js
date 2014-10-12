9d6a4e6fb3a9403f8f860da4a83e23ac5b8ca628
Jeff Fisher <xirzec@xirzec.com>
2014-10-12 16:01:44 -0700

    Catch up dist.

--- dist/commonjs/Block.js
+++ dist/commonjs/Block.js
@@ -4,6 +4,8 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
+var DomUtils = require('./DomUtils');
+
 ;
 
 (function (BlockType) {
@@ -13,36 +15,50 @@ var __extends = this.__extends || function (d, b) {
     BlockType[BlockType["Block"] = 3] = "Block";
     BlockType[BlockType["IfBlock"] = 4] = "IfBlock";
     BlockType[BlockType["RepeaterBlock"] = 5] = "RepeaterBlock";
+    BlockType[BlockType["View"] = 6] = "View";
 })(exports.BlockType || (exports.BlockType = {}));
 var BlockType = exports.BlockType;
 
 var Block = (function () {
     function Block(view) {
         this.children = [];
+        this.bindings = [];
+        this._lastValues = {};
         this.view = view;
     }
     Block.prototype.render = function () {
         if (!this.elements) {
-            this.elements = renderNodes(this.template);
+            this.elements = renderNodes(this, this.template);
         }
         this.children.forEach(function (child) {
             child.render();
         });
     };
 
-    Block.prototype.attach = function () {
-        this.children.forEach(function (child) {
-            child.attach();
-        });
-    };
+    Block.prototype.bind = function () {
+        this._bindEvents();
 
-    Block.prototype.detach = function () {
         this.children.forEach(function (child) {
-            child.detach();
+            child.bind();
         });
     };
 
     Block.prototype.update = function () {
+        var _this = this;
+        this.bindings.forEach(function (binding) {
+            for (var bindingType in binding) {
+                if (bindingType != 'id' && bindingType != 'events' && bindingType != 'element') {
+                    if (bindingType === 'text' || bindingType === 'html') {
+                        _this._updateViewValue(binding, bindingType, binding[bindingType]);
+                    } else {
+                        for (var bindingDest in binding[bindingType]) {
+                            _this._updateViewValue(binding, bindingType, binding[bindingType][bindingDest], bindingDest);
+                        }
+                    }
+                }
+            }
+        });
+
         this.children.forEach(function (child) {
             child.update();
         });
@@ -53,16 +69,138 @@ var Block = (function () {
             child.dispose();
         });
     };
+
+    Block.prototype._updateViewValue = function (binding, bindingType, sourcePropertyName, bindingDest) {
+        var key = binding.id + bindingType + (bindingDest ? ('.' + bindingDest) : '');
+        var lastValue = this._lastValues[key];
+        var currentValue = this.view.getValue(sourcePropertyName);
+
+        if (lastValue != currentValue) {
+            this._lastValues[key] = currentValue;
+
+            var el = binding.element;
+
+            switch (bindingType) {
+                case 'text':
+                    el.textContent = currentValue;
+                    break;
+
+                case 'html':
+                    el.innerHTML = currentValue;
+                    break;
+
+                case 'css':
+                    el.style[bindingDest] = currentValue;
+                    break;
+
+                case 'className':
+                    DomUtils.toggleClass(el, bindingDest, currentValue);
+                    break;
+
+                case 'attr':
+                    if (bindingDest === "value" || bindingDest === 'checked') {
+                        el[bindingDest] = currentValue;
+                    } else if (currentValue) {
+                        el.setAttribute(bindingDest, currentValue);
+                    } else {
+                        el.removeAttribute(bindingDest);
+                    }
+                    break;
+            }
+        }
+    };
+
+    Block.prototype._bindEvents = function () {
+        var _this = this;
+        this.bindings.forEach(function (binding) {
+            var targetElement = binding.element;
+
+            for (var bindingType in binding) {
+                if (bindingType != 'id' && bindingType != 'events' && bindingType != 'element') {
+                    for (var bindingDest in binding[bindingType]) {
+                        var source = binding[bindingType][bindingDest];
+                        if (source.indexOf('$parent') > -1) {
+                            _this.view.viewModel.setData({
+                                '$parent': (_this.view.owner || _this.view.parent).viewModel
+                            }, false);
+                        }
+                        if (source.indexOf('$root') > -1) {
+                            _this.view.viewModel.setData({
+                                '$root': _this.view._getRoot().viewModel
+                            }, false);
+                        }
+                    }
+                }
+            }
+
+            if (binding.events) {
+                for (var eventName in binding.events) {
+                    var targetList = binding.events[eventName];
+
+                    _this._bindEvent(targetElement, eventName, targetList);
+                }
+            }
+
+            _this._bindInputEvent(targetElement, binding);
+        });
+    };
+
+    Block.prototype._bindInputEvent = function (element, binding) {
+        if (binding.attr && (binding.attr.value || binding.attr.checked)) {
+            this.view.activeEvents.on(element, 'input,change', function () {
+                var source = binding.attr.value ? 'value' : 'checked';
+                var newValue = element[source];
+                var key = binding.id + 'attr.' + source;
+
+                this._lastValues[key] = newValue;
+                this.setValue(binding.attr[source], newValue);
+
+                return false;
+            });
+        }
+    };
+
+    Block.prototype._bindEvent = function (element, eventName, targetList) {
+        var _this = this;
+
+        if (eventName.indexOf('$view.') == 0) {
+            eventName = eventName.substr(6);
+            element = this;
+        }
+
+        this.view.activeEvents.on(element, eventName, function (ev) {
+            var returnValue;
+
+            for (var targetIndex = 0; targetIndex < targetList.length; targetIndex++) {
+                var target = targetList[targetIndex];
+                var args = arguments;
+
+                returnValue = this._getValueFromFunction(target, args);
+            }
+
+            return returnValue;
+        });
+    };
+
+    Block.prototype._processBinding = function (spec, element) {
+        if (spec.binding) {
+            spec.binding.id = this.bindings.length.toString();
+            spec.binding.element = element;
+            this.bindings.push(spec.binding);
+        }
+
+        return element;
+    };
     return Block;
 })();
 exports.Block = Block;
 
-function renderNodes(nodes) {
+function renderNodes(block, nodes) {
     if (nodes) {
         return nodes.map(function (node) {
-            var children = renderNodes(node.children);
             if (node.type === 0 /* Element */) {
-                return createElement(node.tag, node.attr, children);
+                var children = renderNodes(block, node.children);
+                return block._processBinding(node, createElement(node.tag, node.attr, children));
             } else if (node.type === 1 /* Text */) {
                 return createText(node.value);
             } else if (node.type === 2 /* Comment */) {
@@ -71,6 +209,8 @@ function renderNodes(nodes) {
                     node.owner.placeholder = c;
                 }
                 return c;
+            } else if (node.type === 6 /* View */) {
+                return block.view[node.name].render();
             }
         });
     }
@@ -82,6 +222,7 @@ var IfBlock = (function (_super) {
         _super.call(this, view);
         this.inserted = false;
         this.rendered = false;
+        this.bindCalled = false;
 
         this.source = source;
     }
@@ -90,6 +231,16 @@ var IfBlock = (function (_super) {
             _super.prototype.render.call(this);
             this.insert();
             this.rendered = true;
+            if (this.bindCalled) {
+                _super.prototype.bind.call(this);
+            }
+        }
+    };
+
+    IfBlock.prototype.bind = function () {
+        this.bindCalled = true;
+        if (this.rendered) {
+            _super.prototype.bind.call(this);
         }
     };
 
@@ -103,17 +254,21 @@ var IfBlock = (function (_super) {
                 this.render();
             }
         } else if (!condition && this.inserted) {
-            this.detach();
             this.remove();
+        }
+
+        if (condition) {
+            _super.prototype.update.call(this);
         }
     };
 
     IfBlock.prototype.insert = function () {
-        var _this = this;
         if (!this.inserted) {
             this.inserted = true;
+            var lastElement = this.placeholder;
             this.elements.forEach(function (element) {
-                insertAfter(element, _this.placeholder);
+                insertAfter(element, lastElement);
+                lastElement = element;
             });
         }
     };
@@ -152,7 +307,7 @@ exports.RepeaterBlock = RepeaterBlock;
 
 function fromSpec(view, spec) {
     var block;
-    if (spec.type === 0 /* Element */ || spec.type === 1 /* Text */) {
+    if (spec.type === 0 /* Element */ || spec.type === 1 /* Text */ || spec.type === 6 /* View */) {
         block = new Block(view);
         block.template = processTemplate(block, [spec]);
     } else {
@@ -187,7 +342,7 @@ function processTemplate(parent, template) {
             if (spec.children) {
                 spec.children = processTemplate(parent, spec.children);
             }
-        } else {
+        } else if (spec.type !== 6 /* View */) {
             var block = createBlock(parent.view, spec);
             block.template = processTemplate(block, spec.children);
             parent.children.push(block);
