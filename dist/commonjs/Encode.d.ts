8195d7f7a56ce2249528d67e751c33edb7166ff7
David Zearing <david@davids-mbp-2.home.network>
2014-10-12 21:44:41 -0700

    Removing dist.

--- dist/commonjs/Encode.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Encode {
-    static toHtml(val: string): string;
-    static toHtmlAttr(val: string): string;
-    static toJS(val: string): string;
-    static toUrl(val: string): string;
-    static toSafe(val: string): string;
-}
-export = Encode;
