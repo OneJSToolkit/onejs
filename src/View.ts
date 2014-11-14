700376404145a3e093ae18a6c58fedc9ebe37823
David Zearing <dzearing@microsoft.com>
2014-11-13 18:53:44 -0800

    Revert "Fixing the removal of $root; you can now hit $root.someViewModelValue to get root's vm value. Adding "existingargs" to end of arguments list."
    This reverts commit 8d0abfa5e162704358d16fd0b5abf7b0f7d83fac.

--- src/View.ts
+++ src/View.ts
@@ -88,16 +88,12 @@ class View extends BaseView {
 
     findValue(args) {
         var resource = this.getValue(args.name);
-        var continueFind = true;
 
         if (resource === undefined && this.parent && this.parent['findValue']) {
             this.parent['findValue'](args);
         } else {
             args.val = resource;
-            continueFind = false;
         }
-
-        return continueFind;
     }
 
     setValue(propertyName: string, propertyValue: any) {
@@ -165,9 +161,6 @@ class View extends BaseView {
             if (props[0] == 'parent') {
                 propTarget = this.parent;
                 props.shift();
-            } else if (props[0] == 'root') {
-                propTarget = this._getRoot();
-                props.shift();
             } else if (props[0] == 'owner') {
                 if (this.owner) {
                     propTarget = viewModel = (this.owner).viewModel;
@@ -179,10 +172,6 @@ class View extends BaseView {
                 props.shift();
             } else {
                 propTarget = this.owner || this;
-                if (propTarget && props[0] && propTarget[props[0]].viewModel) {
-                    propTarget = viewModel = propTarget[props[0]].viewModel;
-                    props.shift();
-                }
             }
         } else {
             while (scopeSource) {
@@ -206,10 +195,14 @@ class View extends BaseView {
                 prop = props[i] = prop.substr(0, parenIndex);
             }
 
-            if (propTarget && propTarget.viewModel) {
+            if (i < (props.length - 1)) {
+                propTarget = propTarget[prop];
 
-                // this vm should be observed.
-                propTarget = viewModel = propTarget.viewModel;
+                if (propTarget && propTarget.viewModel) {
+
+                    // this vm should be observed.
+                    propTarget = viewModel = propTarget.viewModel;
+                }
             }
         }
 
@@ -255,12 +248,12 @@ class View extends BaseView {
             }
         }
 
-        if (existingArgs) {
+        if (args.length == 0 && existingArgs) {
             if (existingArgs && existingArgs.length == 1 && existingArgs[0].args) {
-                args = args.concat([ existingArgs[0].args ]);
+                args = [ existingArgs[0].args ];
             }
             else {
-                args = args.concat(existingArgs);
+                args = existingArgs;
             }
         }
 
