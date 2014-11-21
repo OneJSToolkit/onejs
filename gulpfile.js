2a00e78b6841ec421aaefb1a07bff02f86b8da43
Bob Nisco <bobnisco@gmail.com>
2014-11-21 14:25:12 -0800

    Start to utlize new common gulp from gulp-onejs-compiler

--- gulpfile.js
+++ gulpfile.js
@@ -4,69 +4,15 @@ var coveralls = require('gulp-coveralls');
 var tsc = require('gulp-typescript');
 var karma = require('karma').server;
 var del = require('del');
+var oneJsCompiler = require('gulp-onejs-compiler');
 
-var paths = {
-    source: ['src/*.ts']
-};
+oneJsCompiler.gulpTasks.all({
+    gulp: gulp,
+    rootDir: __dirname,
+    karma: karma
+});
 
 var shouldExit = true;
-
-gulp.task('clean', function(cb) {
-    del(['dist/'], cb);
-});
-
-gulp.task('tscAMD', ['clean'], function() {
-    var tsResult = gulp.src(paths.source)
-        .pipe(tsc({
-            module: 'amd',
-            target: 'ES5',
-            declarationFiles: true
-        }));
-
-    
-    tsResult.dts.pipe(gulp.dest('dist/amd'));
-
-    return tsResult.js.pipe(gulp.dest('dist/amd'));
-});
-
-gulp.task('tscCommonJS', ['clean'], function() {
-    var tsResult = gulp.src(paths.source)
-        .pipe(tsc({
-            module: 'commonjs',
-            target: 'ES5',
-            declarationFiles: true
-        }));
-
-    tsResult.dts.pipe(gulp.dest('dist/commonjs'));
-
-    return tsResult.js.pipe(gulp.dest('dist/commonjs'));
-});
-
-gulp.task('cleanTest', function(cb) {
-    del(['bin'], cb);
-});
-
-gulp.task('copyDist', ['tscCommonJS'], function() {
-    return gulp.src('dist/commonjs/*.js')
-        .pipe(gulp.dest('bin/src'));
-});
-
-gulp.task('tscTest', ['cleanTest', 'copyDist'], function() {
-    var tsResult = gulp.src('test/*.ts')
-        .pipe(tsc({
-            module: 'commonjs',
-            target: 'ES5'
-        }));
-
-    return tsResult.js.pipe(gulp.dest('bin/test'));
-});
-
-gulp.task('test', ['tscTest'], function (done) {
-  karma.start({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done);
-});
 
 gulp.task('tdd', [], function (done) {
   karma.start({
@@ -92,9 +38,7 @@ gulp.doneCallback = function(err) {
     }
 }
 
-gulp.task('default', ['tscAMD', 'tscCommonJS']);
-
 gulp.task('watch', ['default'], function() {
     shouldExit = false;
-    return gulp.watch('src/**/*', ['default']);
+    return gulp.watch(oneJsCompiler.gulpTasks.paths.src.glob, ['default']);
 });
